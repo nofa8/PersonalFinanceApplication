f2286cd2a69b78b4e6c0d7708bf596d8be019896
Rafael Perdigão <2221442@my.ipleiria.pt>
2024-04-11 10:23:15 +0100

    CreateAcc page design done

---
+++ src/main/java/Ficha4/CreateAccPage.java
@@ -0,0 +1,13 @@
+package src.main.java.Ficha4;
+
+import javax.swing.*;
+
+public class CreateAccPage {
+    private JPanel panel1;
+    private JButton createAccountButton;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JButton backButton;
+
+
+}
---
+++ src/main/java/Ficha4/RegistsPage.java
@@ -0,0 +1,4 @@
+package src.main.java.Ficha4;
+
+public class RegistsPage {
+}
