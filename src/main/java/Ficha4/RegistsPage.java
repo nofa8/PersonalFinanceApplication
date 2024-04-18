e1c8bc7bb31f0877293d416f05de12776dd42f8c
Rafael Perdigão <2221442@my.ipleiria.pt>
2024-04-18 10:22:57 +0100

    registers

--- src/main/java/Ficha4/RegistsPage.java
+++ src/main/java/Ficha4/RegistsPage.java
@@ -4,5 +4,11 @@ import javax.swing.*;
 
 public class RegistsPage extends JFrame{
 
-
+    private Registo selected;
+    private JList list1;
+    private JPanel panel1;
+    private JButton backButton;
+    private JButton createButton;
+    private JButton editButton;
+    private JButton deleteButton;
 }
