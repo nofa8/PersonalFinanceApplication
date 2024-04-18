18602ea356468cd0ccd305ebf26c8f941e5de9c5
luiss <2221441@my.ipleiria.pt>
2024-04-18 10:38:47 +0100

    Amo pisa

--- src/main/java/Ficha4/UserPage.java
+++ src/main/java/Ficha4/UserPage.java
@@ -1,4 +1,11 @@
 package Ficha4;
 
+import javax.swing.*;
+
 public class UserPage {
+    private JLabel userName;
+    private JButton logoutButton;
+    private JButton backButton;
+    private JTextArea userDetails;
+    private JButton settingsButton;
 }
