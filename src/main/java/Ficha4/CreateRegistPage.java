ae82cc3ba4748878c7b8986505e297515092304c
Rafael Perdigão <2221442@my.ipleiria.pt>
2024-04-18 10:06:12 +0100

    gsgsg

--- src/main/java/Ficha4/CreateRegistPage.java
+++ src/main/java/Ficha4/CreateRegistPage.java
@@ -24,7 +24,6 @@ public class CreateRegistPage  extends JFrame {
 
         comboBox1.addItem("Profit");
 
-
         comboBox1.addItem("Loss");
 
         for (Categoria categoria : Categoria.values()) {
